78bac02fc38c9b6b37c6c21deaee3dec2e5e9c1b
eitan-ker <58944748+eitan-ker@users.noreply.github.com>
2020-06-13 16:08:15 +0300

    changed typeURL to text - next: need to make a connection

--- app/src/main/java/com/example/flightmobileapp/MainActivity.kt
+++ app/src/main/java/com/example/flightmobileapp/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.flightmobileapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun connect(view: View): Unit {
+        connectButton.setOnClickListener() {
+
+        }
     }
 }
